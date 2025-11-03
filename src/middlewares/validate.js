4c5181368b7511d650119531be540786480a284c
Isabella Maria <imbrodrigues.mbr@gmail.com>
2025-11-03 20:33:46 -0300

    implement userschema validation

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -1,6 +1,10 @@
 export const validade = (schemas = {}) => (request, _response, next) => {
     try {
-
+            if(schemas.body) request.body = schemas.body.parse(request.body)
+            if(schemas.query) request.query = schemas.body.parse(request.query)
+            if(schemas.params) request.params = schemas.body.parse(request.params)
+       
+                return next()
     } catch (error) {
         const issues = error?.issues?.map(item => ({
          path: item.path,
